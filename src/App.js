a082b96035d6780d3212925736ad683fa22d60cc
Juan Sebastian <juan_seb07@hotmail.com>
2022-07-19 23:26:51 -0500

    Se agrega el logo y se importa, adicionalmente se crean estilos base.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import jsbmLogo from './img/logo-jsbm-medium.png';
 
 function App() {
   return (
-    <div className="App">
-     Hola
+    <div className='App'>
+     <div className='jsbm__logo__contenedor'>
+      <img 
+        src={jsbmLogo}
+        className='jsbm__logo'
+        alt='Logo de JSBM' />
+     </div>
     </div>
   );
 }
